c9e185bee937809ce24d87189c7881c0f564efa8
Kaique <kiaqbs@gmail.com>
2024-12-28 13:37:38 -0300

    treinos

---
+++ treinos/form/index.js
@@ -0,0 +1,5 @@
+const form = document.getElementById('form');
+const email = document.getElementById('email');
+const emailRegex = /^[a-z0-9.]+@[a-z0-9]+\.[a-z]+\.[a-z]?$/i;
+const password = document.getElementById('senha');
+const resultMessage = document.getElementById('resultMessage')
